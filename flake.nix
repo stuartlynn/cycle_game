d360855f1f01bc019bba3b1fecfef6e73fab000c
Stuart Lynn <stuart.lynn@gmail.com>
2024-07-21 15:48:20 +0100

    flaky goodness

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "A game about cycles";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = { self, flake-utils, rust-overlay, nixpkgs }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = (import nixpkgs) {
+          inherit system overlays;
+        };
+      in
+      with pkgs;
+      {
+        devShells.default = mkShell rec {
+          nativeBuildInputs = [
+            (rust-bin.stable.latest.default.override {
+              extensions = [ "rust-analyzer" "clippy" "rust-src" ];
+            })
+          ];
+          buildInputs = [
+            udev alsa-lib vulkan-loader
+            xorg.libX11 xorg.libXcursor xorg.libXi xorg.libXrandr
+            libxkbcommon wayland pkg-config
+          ];
+          LD_LIBRARY_PATH = lib.makeLibraryPath buildInputs;
+        };
+      }
+    );
+}
